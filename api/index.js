534706406f68c134f4f14cb9bd41d56bc55994be
engsahadat <eng.md.sahadathossain@gmail.com>
2026-06-07 01:01:00 +0600

    hydfct

--- api/index.js
+++ api/index.js
@@ -3,7 +3,3 @@ import server from "../dist/server/server.js";
 export default async function handler(request) {
   return server.fetch(request);
 }
-
-export const config = {
-  runtime: "edge",
-};
